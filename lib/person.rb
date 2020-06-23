4ab11c1823834e77084ad8e7010606bf12691136
Eve Reichmann <eve.reichmann@gmail.com>
2020-06-23 11:06:44 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+#Person
+#  is defined (FAILED - 6)
+#  #talk
+#    responds to the #talk instance method (FAILED - 7)
+#    puts "Hello World!" (FAILED - 8)
+#  #walk
+#    responds to the #walk instance method (FAILED - 9)
+#    puts "The Person is walking" (FAILED - 10)
+
+class Person
+    def talk
+        puts "Hello World!"
+    end
+    def walk
+        puts "The Person is walking"
+    end
+end
